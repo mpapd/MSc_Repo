94c233ee6a5e73acf8413706116dbec0eb77598a
mpapd <valkon.gr@gmail.com>
2019-12-21 15:32:31 +0200

    Backend project init

---
+++ Α' Εξάμηνο/Διαχείριση Γνώσης/Ομαδικό_Project/eregister-backend/src/test/java/com/papadimitriou_georgiadis/eregisterbackend/EregisterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papadimitriou_georgiadis.eregisterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EregisterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
